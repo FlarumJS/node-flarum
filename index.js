342a55cf5eeb92f8a310d789fa596bdd113811d1
David Sevilla Martín <dsevilla192@gmail.com>
2016-01-24 20:07:04 -0500

    No clue what i'm doing...

--- index.js
+++ index.js
@@ -1,31 +1,114 @@
+var express = require('express');
+var hbs = require('hbs');
+var debug = require('debug')('flarum:init');
+var debugError = require('debug')('flarum:error');
+
+var routes = require('./lib/routes/index');
+var config = require('./lib/config/config.json');
+
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+var app = module.exports = express.Router();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set('port', process.env.PORT || 8080);
+
+hbs.registerPartials(__dirname + '/lib/views/partials');
+app.use(express.static(path.join(__dirname, '/lib/public')));
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		if (err.status != 404) {
+			debugError('[ERROR]')
+			debug(err)
+		}
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err,
+			notFound: err.status == 404,
+			serverError: err.status == 500
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		notFound: err.status == 404,
+		serverError: err.status == 500,
+		error: { status: err.status }
+	});
+});
+
+if (config.mongodb.host && config.mongodb.database &&
+	!config.mongodb.username && !config.mongodb.password) {
+	var mongoUrl = 'mongodb://' + config.mongodb.host + '/' + config.mongodb.database;
+mongoose.connect(mongoUrl);
+} else if (config.mongodb.host && config.mongodb.database &&
+	config.mongodb.username && config.mongodb.password) {
+	var mongoUrl = 'mongodb://' + config.mongodb.username + ':';
+	mongoUrl += config.mongodb.password + '@';
+	mongoUrl += config.mongodb.host + '/' + config.mongodb.database;
+	mongoose.connect(mongoUrl);
+}
+
+db.on('error', function (err) {
+	if (err == 'MongoError: connect ECONNREFUSED') {
+		debugError('[MongoDB] Could not connect to MongoDB!!');
+	} else {
+		debugError('[MongoDB] ' + err);
+	}
+});
+
+
 /**
  * Escape special characters in the given string of html.
  *
  * @param  {String} html
  * @return {String}
  */
-module.exports = {
-  escape: function(html) {
-    return String(html)
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
-  },
+// module.exports = {
+//   escape: function(html) {
+//     return String(html)
+//       .replace(/&/g, '&amp;')
+//       .replace(/"/g, '&quot;')
+//       .replace(/'/g, '&#39;')
+//       .replace(/</g, '&lt;')
+//       .replace(/>/g, '&gt;');
+//   },
+	// /**
+	//  * Unescape special characters in the given string of html.
+	//  *
+	// * @param  {String} html
+	// * @return {String}
+	// */
+//   unescape: function(html) {
+//     return String(html)
+//       .replace(/&amp;/g, '&')
+//       .replace(/&quot;/g, '"')
+//       .replace(/&#39;/g, '\'')
+//       .replace(/&lt;/g, '<')
+//       .replace(/&gt;/g, '>');
+//   }
+// };
 
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&#39;/g, '\'')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
-  }
-};
+
+
