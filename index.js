abf8d71ba872ef112885ea16dc2b427609fa6997
David Sevilla Martín <dsevilla192@gmail.com>
2016-01-25 18:10:13 -0500

    tweaked some things with mongo to connect right after install

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.use(function(req, res, next) {
 	next(err);
 });
 
-fs.exists(path.join(__dirname + 'lib/config/config.json'), function (exists) {
+
+fs.exists(__dirname + '/lib/config/config.json', function (exists) {
 	if (!exists) {
 		console.log('File does not exist!')
 		fs.writeFile(__dirname + '/lib/config/config.json', '{}', function (err2) {
@@ -39,9 +40,11 @@ fs.exists(path.join(__dirname + 'lib/config/config.json'), function (exists) {
 		config = require('./lib/config/config.json');
 		routes = require('./lib/routes/index');
 		app.use('/', routes);
+		console.log('Path: ' + routes.path());
 	}
 
 })
+
 
 // error handlers
 
@@ -99,38 +102,3 @@ db.on('error', function (err) {
 		debugError('[MongoDB] ' + err);
 	}
 });
-
-
-/**
- * Escape special characters in the given string of html.
- *
- * @param  {String} html
- * @return {String}
- */
-// module.exports = {
-//   escape: function(html) {
-//     return String(html)
-//       .replace(/&/g, '&amp;')
-//       .replace(/"/g, '&quot;')
-//       .replace(/'/g, '&#39;')
-//       .replace(/</g, '&lt;')
-//       .replace(/>/g, '&gt;');
-//   },
-	// /**
-	//  * Unescape special characters in the given string of html.
-	//  *
-	// * @param  {String} html
-	// * @return {String}
-	// */
-//   unescape: function(html) {
-//     return String(html)
-//       .replace(/&amp;/g, '&')
-//       .replace(/&quot;/g, '"')
-//       .replace(/&#39;/g, '\'')
-//       .replace(/&lt;/g, '<')
-//       .replace(/&gt;/g, '>');
-//   }
-// };
-
-
-
