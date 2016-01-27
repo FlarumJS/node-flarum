7d3089acd7066f5322d5d17a970910edfe0dfead
David Sevilla Martín <dsevilla192@gmail.com>
2016-01-27 18:51:26 -0500

    [FIXED] Custom error page not being used, using parent one;

--- index.js
+++ index.js
@@ -65,46 +65,7 @@ fs.readFile('flarum/config.json', 'utf8', function (err, data) {
 
 		if (config.mongodb) connectMongo(config.mongodb);
 	}
-	app.use(function(req, res, next) {
-		var err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});
 })
-
-
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		if (err.status != 404) {
-			debugError('[ERROR]')
-			debug(err)
-		}
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err,
-			notFound: err.status == 404,
-			serverError: err.status == 500
-		});
-	});
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		notFound: err.status == 404,
-		serverError: err.status == 500,
-		error: { status: err.status }
-	});
-});
 
 
 db.on('error', function (err) {
