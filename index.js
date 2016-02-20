0bfcddbdd214c58f4ec05d6e42532747b2d51431
David Sevilla Martín <dsevilla192@gmail.com>
2016-02-20 10:04:57 -0500

    Installed (NPM) express-session, passport, passport-local, passport-google;

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var debugError = require('debug')('flarum:error');
 var fs = require('fs');
 var config, routes;
 
+var passport = require('./lib/config/passport')
+var functions = require('./lib/config/functions');
+
 
 // var configFileDirname = path.join(__dirname + '/../../flarum/config.json'); // production
 var configFileDirname = path.join(__dirname + '/flarum/config.json'); // development
 
-var functions = require('functions');
 var connectMongo = functions.connectMongo;
 
 var mongoose = require('mongoose');
@@ -23,20 +25,25 @@ app.set('view engine', 'hbs');
 app.set('port', process.env.PORT || 8080);
 
 hbs.registerPartials(__dirname + '/lib/views/partials');
+
+
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, '/lib/public')));
 
 
 fs.readFile(configFileDirname, 'utf8', function (err, data) {
-
+	
 	if (!err && !data) {
 		fs.writeFile('flarum/config.json', '{}', function (err2) {
 			if (err2) throw err2;
 			console.log('[FLARUM] Config File Set Up!');
-
+			
 			config = require(configFileDirname);
-
+			
 			if (config.mongodb) connectMongo(config.mongodb);
-
+			
 			setUpRoutes();
 			app.use('/', routes);
 		});
@@ -52,11 +59,11 @@ fs.readFile(configFileDirname, 'utf8', function (err, data) {
 					return console.log('[FLARUM] ' + err);
 				} else if (err2) throw err2;
 				console.log('[FLARUM] Config File Written!');
-
+				
 				config = require(configFileDirname);
-
+				
 				if (config.mongodb) connectMongo(config.mongodb);
-
+				
 				setUpRoutes();
 				app.use('/', routes);
 			});
@@ -65,15 +72,17 @@ fs.readFile(configFileDirname, 'utf8', function (err, data) {
 		var error =  new Error('[FLARUM] ' + err);
 		throw error;
 	} else if (data) {
-
+		
 		config = require(configFileDirname);
-
+		
 		setUpRoutes()
 		app.use('/', routes);
-
+		
 		if (config.mongodb) connectMongo(config.mongodb);
 	}
 })
+
+
 
 
 db.on('error', function (err) {
